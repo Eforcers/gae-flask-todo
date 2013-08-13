be482d8d6ceed32f94ece03bf70de0ec8052bd17
carlos.prieto <carlos.prieto@eforcers.com>
2013-08-13 09:23:56 -0500

    el servicio se encarga de todo lo relacionado con la comunicacion con los endpoints siendo cargado de manera dinamica

--- todo_app/static/js/angular/controllers.js
+++ todo_app/static/js/angular/controllers.js
@@ -1,60 +1,53 @@
 
-todoCtrl = Todo_app.controller('todoCtrl', function ($window, $scope, TodoService) {
+todoCtrl = Todo_app.controller('todoCtrl', function ($window, $scope, EndpointService) {
     $window.init = function() {
       $scope.$apply($scope.load_todo_api);
     };
 
-    $scope.$watch(TodoService.isLoaded, function(loaded) {
-        $scope.loaded = loaded;
-    });
-
     $scope.load_todo_api = function() {
-        var apiRoot = '//' + window.location.host + '/_ah/api';
-        gapi.client.load('todo', 'v1', function() {
-            $scope.is_api_ready = true;
-            $scope.loadTodos();
-        }, apiRoot);
+        EndpointService.loadService('todo', 'v1');
     };
 
+    $scope.$watch(EndpointService.isLoaded, function(loaded) {
+        $scope.loaded = loaded;
+        if (loaded){
+            $scope.loadTodos();
+        }
+    });
 
-	$scope.loadTodos = function(){
-		var promise = TodoService.listtodos();
-		promise.then(function(resp) {
-			$scope.todos = resp.items
-		});
-	};
-	
-	$scope.addTodo = function(){
-		var promise = TodoService.inserttodo({'title':$scope.newTitle});
-		promise.then(function(todo) {
-			$scope.newTitle = ""
+    $scope.loadTodos = function(){
+        var promise = EndpointService.listtodos();
+        promise.then(function(resp) {
+            $scope.todos = resp.items
+        });
+    };
+
+    $scope.addTodo = function(){
+        var promise = EndpointService.inserttodo({'title':$scope.newTitle});
+        promise.then(function(todo) {
+            $scope.newTitle = ""
             setTimeout(function(){
                 $scope.loadTodos();
             }, 500);
 
-		});
-		
-	};
+        });
+    };
 
     $scope.changeTodo = function(todo){
-		var promise = TodoService.toggletodo({'id':todo.id});
-		promise.then(function(todo) {
-			console.log("task was toggle",todo);
-		});
-	};
+        var promise = EndpointService.toggletodo({'id':todo.id});
+        promise.then(function(todo) {
+            console.log("task was toggle",todo);
+        });
+    };
 
     $scope.delTodo = function(todo){
-		var promise = TodoService.deletetodo({'id':todo.id});
-		promise.then(function(todo) {
-			setTimeout(function(){
+        var promise = EndpointService.deletetodo({'id':todo.id});
+        promise.then(function(todo) {
+            setTimeout(function(){
                 $scope.loadTodos();
             }, 500);
-		});
-	};
-
-
-	
-	//$scope.loadTodos();
+        });
+    };
 });
 
-todoCtrl.$inject = ['$window','$scope','TodoService'];
+todoCtrl.$inject = ['$window','$scope','EndpointService'];
--- todo_app/static/js/angular/services.js
+++ todo_app/static/js/angular/services.js
@@ -1,6 +1,7 @@
-Todo_app.service('TodoService',function($q, $rootScope,$http) {
+Todo_app.service('EndpointService',function($q, $rootScope,$http) {
 
     var service = this;
+    var http = $http;
     var builder = function (api, resource, method){
         return function (args) {
               var deferred = $q.defer();
@@ -12,15 +13,25 @@ Todo_app.service('TodoService',function($q, $rootScope,$http) {
     }
     var loaded = false;
     this.isLoaded = function() { return loaded; };
-    $http.get('/_ah/api/discovery/v1/apis/todo/v1/rest').success( function(data) {
-        console.log(data);
-        for (resource in data.resources){
-            for(method in data.resources[resource].methods){
-                service[method+resource] = builder('todo',resource,  method);
-                 console.log("Method "+method+resource+" created");
-            }
-        }
-        loaded = true;
-        $rootScope.$$phase || $rootScope.$apply();
-    });
+
+    this.loadService = function(api, version){
+        service.apiName = api;
+        service.apiVersion = version;
+        var apiRoot = '//' + window.location.host + '/_ah/api';
+        gapi.client.load(service.apiName, service.apiVersion, function() {
+            var apiUrl = '';
+            http.get(apiRoot+'/discovery/v1/apis/'+service.apiName+'/'+service.apiVersion+'/rest').success( function(data) {
+                console.log(data);
+                for (resource in data.resources){
+                    for(method in data.resources[resource].methods){
+                        service[method+resource] = builder(service.apiName,resource,  method);
+                        console.log("Method "+method+resource+" created");
+                    }
+                }
+                loaded = true;
+                //$rootScope.$$phase || $rootScope.$apply();
+            });
+            $rootScope.$$phase || $rootScope.$apply();
+        }, apiRoot);
+    }
 });
